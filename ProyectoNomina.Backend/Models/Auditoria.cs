879e3703e28f48976e3193bdf040782d60922aa9
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-25 17:42:15 -0600

    Se Crearon todos los modelos listos para la Migracion a la base de datos

---
+++ ProyectoNomina.Backend/Models/Auditoria.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoNomina.Backend.Models
+{
+    public class Auditoria
+    {
+        public int Id { get; set; }
+        public string Accion { get; set; }
+        public string Usuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Detalles { get; set; }
+    }
+}
+
---
+++ ProyectoNomina.Backend/Models/Rol.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+namespace ProyectoNomina.Backend.Models;
+
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        [JsonIgnore] // ✅ Evita ciclos infinitos o errores de serialización
+        public ICollection<UsuarioRol> UsuarioRoles { get; set; } = new List<UsuarioRol>();
+    }
+
+
