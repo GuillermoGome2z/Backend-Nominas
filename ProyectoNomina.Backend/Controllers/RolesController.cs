c5d54afe017a7031b25a86e46f781e3cb72f14b6
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-25 21:57:58 -0600

    Se creo el controlador para los Roles

---
+++ ProyectoNomina.Backend/Controllers/RolesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoNomina.Backend.Data;
+using ProyectoNomina.Backend.Models;
+
+namespace ProyectoNomina.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RolesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Roles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rol>>> GetRoles()
+        {
+            return await _context.Roles.ToListAsync();
+        }
+
+        // GET: api/Roles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rol>> GetRol(int id)
+        {
+            var rol = await _context.Roles.FindAsync(id);
+            if (rol == null) return NotFound();
+            return rol;
+        }
+
+        // POST: api/Roles
+        [HttpPost]
+        public async Task<ActionResult<Rol>> PostRol(Rol rol)
+        {
+            _context.Roles.Add(rol);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRol), new { id = rol.Id }, rol);
+        }
+
+        // PUT: api/Roles/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRol(int id, Rol rol)
+        {
+            if (id != rol.Id) return BadRequest();
+
+            _context.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Roles.Any(r => r.Id == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Roles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRol(int id)
+        {
+            var rol = await _context.Roles.FindAsync(id);
+            if (rol == null) return NotFound();
+
+            _context.Roles.Remove(rol);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
