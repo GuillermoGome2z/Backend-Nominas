7774cc2e80b7707657b945fe1d557877ab6a4628
GuillermoGome2z <gomezguille391@.gmailcom>
2025-05-29 11:06:51 -0600

    Se integran archivos al Shared para poder hacer el login de manera correcta

---
+++ ProyectoNomina.Backend/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using ProyectoNomina.Shared.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProyectoNomina.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public ActionResult<LoginResponse> Login([FromBody] LoginRequest request)
+        {
+            // Usuario fijo para pruebas
+            if (request.Correo == "admin@empresa.com" && request.Contraseña == "Admin123!")
+            {
+                var token = GenerarToken(request.Correo, "Admin");
+
+                return Ok(new LoginResponse
+                {
+                    Token = token,
+                    NombreUsuario = "Administrador",
+                    Rol = "Admin"
+                });
+            }
+
+            return Unauthorized("Credenciales inválidas");
+        }
+
+        private string GenerarToken(string correo, string rol)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:SecretKey"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, correo),
+                new Claim(ClaimTypes.Role, rol),
+                new Claim(ClaimTypes.Name, "Administrador")
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JwtSettings:Issuer"],
+                audience: _config["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
+
