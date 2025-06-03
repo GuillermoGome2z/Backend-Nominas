3ec562d8dc3fcd8dadd3a08781881f11dbe81941
GuillermoGome2z <gomezguille391@.gmailcom>
2025-06-03 17:37:26 -0600

    Se agregaron botones para el inicio de sesion y se pusieron los compos obligatorios para crear el usuario

--- ProyectoNomina.Shared/Models/DTOs/RegistrarUsuarioDto.cs
+++ ProyectoNomina.Shared/Models/DTOs/RegistrarUsuarioDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks; 
@@ -8,9 +9,17 @@ namespace ProyectoNomina.Shared.Models.DTOs
 {
     public class RegistrarUsuarioDto
     {
+        [Required(ErrorMessage = "El nombre completo es obligatorio.")]
         public string NombreCompleto { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Correo inválido.")]
         public string Correo { get; set; } = string.Empty;
-        public string Clave  { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        public string Clave { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un rol.")]
         public int RolId { get; set; }
     }
 }
