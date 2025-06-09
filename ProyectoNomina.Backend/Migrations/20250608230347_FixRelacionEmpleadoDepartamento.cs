67dca3db8fcd08185c70afe0f41c335b60ef2cd8
GuillermoGome2z <gomezguille391@.gmailcom>
2025-06-08 18:15:45 -0600

    Se relacionaron los modelos de empleado y departamento

---
+++ ProyectoNomina.Backend/Migrations/20250608230347_FixRelacionEmpleadoDepartamento.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoNomina.Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRelacionEmpleadoDepartamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
